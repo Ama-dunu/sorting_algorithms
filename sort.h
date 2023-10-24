478638ac9cca7620d7a10fdf5e48f75e5b22c255
Ama-dunu <annettebdunu90@gmail.com>
2023-10-24 15:33:10 -0700

    0x1B. C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -52,5 +52,7 @@ int get_max(int *array, int size);
 void merge_sort(int *array, size_t size);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 #endif /* SORT_H */
