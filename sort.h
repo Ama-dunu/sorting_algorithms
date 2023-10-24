060d18cbb3838032e4c87d7ea857ae72eef43a10
Ama-dunu <annettebdunu90@gmail.com>
2023-10-24 15:15:48 -0700

    0x1B. C - Sorting algorithms & Big O

---
+++ sort.h
@@ -0,0 +1,56 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void bubble_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2);
+void selection_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void quick_sort(int *array, size_t size);
+void lomuto_sort(int *array, size_t size, int left, int right);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void swap_ints(int *a, int *b);
+void shell_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+int get_max(int *array, int size);
+void merge_sort(int *array, size_t size);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
+
+#endif /* SORT_H */
