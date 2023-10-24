58307b8cf1ad856c578ad87d4c722babcfc7c67d
Ama-dunu <annettebdunu90@gmail.com>
2023-10-24 16:04:40 -0700

    0x1B. C - Sorting algorithms & Big O

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,9 +7,9 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -23,26 +23,26 @@ void swap_ints(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap, i, j;
+	size_t gap, i, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (gap = 1; gap < size / 3;)
-        gap = gap * 3 + 1;
+	for (gap = 1; gap < size / 3;)
+		gap = gap * 3 + 1;
 
-    for (; gap >= 1; gap /= 3)
-    {
-        for (i = gap; i < size; i++)
-        {
-            j = i;
-            while (j >= gap && array[j - gap] > array[j])
-            {
-                swap_ints(array + j, array + (j - gap));
-                j -= gap;
-            }
-        }
-        print_array(array, size);
-    }
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
 }
 
