f12d918a6de6187df94724eae3d448962f942d64
Ama-dunu <annettebdunu90@gmail.com>
2023-10-25 11:18:56 -0700

    0x1B. C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -54,5 +54,22 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_ints(int *a, int *b);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+void quick_sort_hoare(int *array, size_t size);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_sort(int *array, size_t size);
 
 #endif /* SORT_H */
